05354278e6be2da1c4708bd34e94a14b8b81096b
Grugier <soldatb10@laposte.net>
2023-01-05 17:07:54 +0100

    Bases souvenir statique (open/close)

---
+++ frontend/src/components/Souvenir.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p @click="$emit('fermersouvenir')" class="close">X</p>
+        <p>Bonjour je suis un souvenir lol</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+div {
+    background-color: blueviolet;
+    width: 17rem;
+    height: 12rem;
+    position: absolute;
+    top: 20%;
+    left: 20%;
+}
+.close {
+    cursor: pointer;
+}
+</style>
--- frontend/src/views/VisiteView.vue
+++ frontend/src/views/VisiteView.vue
@@ -1,16 +1,51 @@
 <template>
-    <a-scene background="color: #ECECEC">
-        <a-assets>
+    <a-scene raycaster="objects: .clickable" cursor="rayOrigin: mouse" renderer="colorManagement: true;" background="color: #ECECEC">
+        <!-- <a-assets>
             <a-asset-item id="stgic" src="/BatimentC.glb"></a-asset-item>
         </a-assets>
-        <a-entity static-body></a-entity>
+        <a-entity static-body></a-entity> -->
+
+        <!-- ENVIRONNEMENT DE TEST EN ATTENDANT LE BATIMENT -->
+
+        <!-- Plafond -->
+        <a-plane static-body position="-0.62674 6.967 -4.46685" rotation="90 0 0" width="35.99" height="36.95"
+            color="#ffffff" material="shader: flat;"></a-plane>
+
+        <!-- Sol -->
+        <a-plane static-body position="-0.62674 0 -4.46685" rotation="-90 0 0" width="35.99" height="36.95"
+            color="#999999" shadow></a-plane>
+
+        <!-- Murs -->
+        <a-box static-body position="16.61 3.5 -4.57041" geometry="height: 8; width: 37.27" rotation="0 90 0"></a-box>
+        <a-box static-body position="-17.857 3.5 -4.35469" geometry="height: 8; width: 37.19" rotation="0 90 0"></a-box>
+        <a-box static-body position="-0.43974 3.5 7.604" geometry="height: 8; width: 35.52"></a-box>
+        <a-box static-body position="-0.4882 3.5 -22.5" geometry="height: 8; width: 35.59"></a-box>
+
+        <a-box static-body position="7 .7 0" geometry="height: 2; width: 2"></a-box>
+
+        <a-entity light="type: ambient; intensity: 0.35;"></a-entity>
+        <a-entity light="type: ambient; intensity: 0.35;"></a-entity>
+        <a-entity light="type: directional;
+                             castShadow: true;
+                             intensity: 0.25;" position="-5 3 1.5"></a-entity>
+
+        <!-- FIN ENVIRONNEMENT DE TEST -->
 
         <!-- Caméra -->
-        <a-entity id="joueur" kinema-body="radius: 0.4;" movement-controls="fly: false" position="0 5 -4.531">
+        <a-entity id="joueur" kinema-body="radius: 0.4;" movement-controls="fly: false" position="0 0 -4.531">
             <a-entity camera position="0 1.8 0" look-controls></a-entity>
         </a-entity>
 
+        <!-- Souvenir statique de test -->
+        <a-entity souvenir id="sphere" geometry="primitive: sphere; radius: 0.25"
+                material="color: #99BF1C; shader: flat"
+                position="7 2 0"
+                light="type: point; intensity: 0.075"
+                class="clickable"
+              ></a-entity>
     </a-scene>
+
+    <Souvenir v-show="souvenir.open" @fermersouvenir ="souvenir.open = false"/>
 </template>
 
 <style>
@@ -20,17 +55,27 @@ a-scene {
 }
 </style>
 <script setup>
-import { onMounted } from 'vue'
+import { reactive, onMounted } from 'vue'
+import Souvenir from '../components/Souvenir.vue'
+
+let souvenir = reactive({ open: false})
 
 onMounted(() => {
-    document.querySelector("a-entity").setAttribute("gltf-model", "#stgic");
+    //Pour charger un modèle
+    //document.querySelector("a-entity").setAttribute("gltf-model", "#stgic");
 })
 
-//A-frame
-// AFRAME.registerComponent('batstgi', {
-//     init: function () {
-//         const el = this.el
-//     }
-// });
+//===A-frame===//
+AFRAME.registerComponent("souvenir", {
+    init: function () {
+        const el = this.el;
+
+        //Clique sur le souvenir
+        el.addEventListener("click", function() {
+            console.log('Souvenir cliqué !');
+            souvenir.open = true;
+        });
+    }
+});
 
 </script>
